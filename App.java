c86c30e6ddc09b2e7741e0094ced79a4d46f9029
Luiz Eduardo Ferreira Da Costa Carvalho <19180149@pucrs.br>
2019-08-14 12:38:25 -0300

    changed arraylist for an array. cofrinho's methods were refactored adjusting them to the previous change

--- App.java
+++ App.java
@@ -2,6 +2,26 @@ import java.util.*;
 
 public class App {
     public static void main(String[] args) {
-        
+        Cofrinho c = new Cofrinho();        
+
+        c.inserirMoeda(new Moeda(NomeMoeda.Um));
+        c.inserirMoeda(new Moeda(NomeMoeda.VinteCinco));
+        c.inserirMoeda(new Moeda(NomeMoeda.VinteCinco));
+        c.inserirMoeda(new Moeda(NomeMoeda.VinteCinco));
+
+        System.out.println("Quantidade de moedas: " + c.quantidadeMoedasCofrinho());
+        System.out.println("Valor armazenado em reais: " + c.valorArmazenadoReais());
+        System.out.println("Valor armazenado em centavos: " + c.valorArmazenadoCentavos());
+        System.out.println("Quantidade de moedas de um tipo: " + c.quantidadeMoedasPorTipo(NomeMoeda.VinteCinco));
+
+        if(c.numeroPosOcupadas() > 0) c.retirarUltimaMoeda();
+        if(c.numeroPosOcupadas() > 0) c.retirarUltimaMoeda();
+        if(c.numeroPosOcupadas() > 0) c.retirarUltimaMoeda();
+        if(c.numeroPosOcupadas() > 0) c.retirarUltimaMoeda();
+        if(c.numeroPosOcupadas() > 0) c.retirarUltimaMoeda();
+
+        System.out.println("Quantidade de moedas de um tipo: " + c.quantidadeMoedasPorTipo(NomeMoeda.VinteCinco));
+        System.out.println("Total moedas no cofrinho: " + c.quantidadeMoedasCofrinho());
+
     }
 }
--- Cofrinho.java
+++ Cofrinho.java
@@ -4,7 +4,7 @@ public class Cofrinho {
     
     private final int TAMANHO_COFRINHO = 80;
     Moeda moedas[] = new Moeda[TAMANHO_COFRINHO];
-    private posLivre = 0;
+    private int posLivre = 0;
     private double valorTotal;
 
     public Cofrinho() {
@@ -16,31 +16,49 @@ public class Cofrinho {
         int valor = moedaInserida.getValorCentavos();
         valorTotal += valor;
 
-        moedas
+        // insere moeda inserida na lista
+        moedas[posLivre] = moedaInserida;
+        posLivre++;
 
         return valorTotal;
     }
 
     public Moeda retirarUltimaMoeda() {
-        if(pilha.size() > 0) {
-            Moeda moeda = pilha.pop();
-            valorTotal -= moeda.getValorCentavos();
-            return moeda;
+        
+        if(numeroPosOcupadas() != 0) {
+            NomeMoeda nome = moedas[posLivre-1].getNomeMoeda();
+            moedas[posLivre-1] = null;
+            Moeda moedaASerRetirada = new Moeda(nome);
+            posLivre--;
+
+            return moedaASerRetirada;
         } else {
             return null;
         }
     }
 
     public int quantidadeMoedasCofrinho() {
-        return pilha.size();
+        int cont = 0;    
+
+        for(int i = 0; i < TAMANHO_COFRINHO; i++) {
+            if(moedas[i] != null) {
+                if(moedas[i].getValorCentavos() != 0) {                    
+                    cont++;
+                }
+            }
+        }
+
+        return cont;
     }
 
     public int quantidadeMoedasPorTipo(NomeMoeda nome) {    
         int quantidade = 0;
 
         for(int i = 0; i < TAMANHO_COFRINHO; i++) {
-            if(pilha[i].getNomeMoeda() == nome) {
-                quantidade++;
+            if(moedas[i] != null) {
+                if(moedas[i].getNomeMoeda() == nome) {
+                    quantidade++;
+                }
             }
         }
 
@@ -51,7 +69,9 @@ public class Cofrinho {
         int valor = 0;
 
         for(int i = 0; i < TAMANHO_COFRINHO; i++) {
-            valor += pilha[i].getValorCentavos();
+            if(moedas[i] != null) {
+                valor += moedas[i].getValorCentavos();
+            }
         }
 
         return valor;
@@ -61,9 +81,23 @@ public class Cofrinho {
         double valor = 0;
 
         for(int i = 0; i < TAMANHO_COFRINHO; i++) {
-            valor += pilha[i].getValorReais();
+            if(moedas[i] != null) {
+                valor += moedas[i].getValorReais();
+            }
         }
 
         return valor;
+    }
+
+    public int numeroPosOcupadas() {
+        int cont = 0;
+
+        for(int i = 0; i < TAMANHO_COFRINHO; i++) {
+            if(moedas[i] != null) {
+                cont++;
+            }
+        }
+
+        return cont;
     }
 }
